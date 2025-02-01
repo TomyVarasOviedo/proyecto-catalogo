2bbc4bb66933b36a6346f45f668695b57486601a
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2025-02-01 16:35:19 -0300

    add:<Controllers usuario

--- src/main/java/com/catalogo/proyecto/Models/Usuario.java
+++ src/main/java/com/catalogo/proyecto/Models/Usuario.java
@@ -1,15 +1,19 @@
 package com.catalogo.proyecto.Models;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 
 @Entity(name="usuarios")
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
