3a6ea35375109d58c8288ef14d8397ec3ae61b4d
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2025-02-04 14:57:53 -0300

    add: pedido controller

---
+++ src/main/java/com/catalogo/proyecto/Controllers/PedidoController.java
@@ -0,0 +1,52 @@
+package com.catalogo.proyecto.Controllers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.catalogo.proyecto.Models.Pedido;
+import com.catalogo.proyecto.Services.PedidoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/pedido")
+public class PedidoController {
+    @Autowired
+    private PedidoService servicio;
+
+    @GetMapping("/search")
+    public Pedido getPedidoId(@RequestParam UUID id) {
+        return servicio.getPedidoId(id).get();
+    }
+
+    @GetMapping("/search")
+    public List<Pedido> getMethodName(@RequestParam LocalDateTime fechaInicio, @RequestParam LocalDateTime fechaFin) {
+        return servicio.getPedidoFecha(fechaInicio, fechaFin);
+    }
+    @GetMapping("/all")
+    public List<Pedido> getPedidoall() {
+        return servicio.getPedidoAll();
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<Pedido> guardarPedido(@RequestBody Pedido pedido) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(servicio.realizarPedido(pedido));
+    }
+    
+    @GetMapping("/delete")
+    public ResponseEntity<Pedido> deletePedido(@RequestParam UUID idPedido) {
+        return ResponseEntity.status(HttpStatus.OK).body(servicio.eliminarPedido(idPedido));
+    }
+    
+    
+}
